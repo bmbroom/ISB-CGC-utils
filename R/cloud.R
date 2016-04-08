e7fc0f18cd56c2e172b1c2fd7e573eb02254d53d
Bradley Broom <bmbroom@mdanderson.org>
2016-04-08 10:40:35 -0500

    Removed cloudServer function obsoleted by chmCreateManagedServer

--- R/cloud.R
+++ R/cloud.R
@@ -2,14 +2,3 @@
 getCloudProject <- function() {
    getOption('cloudproject')
 }
-
-#' @export
-cloudServer <- function (name, intAddr, extAddr, chmPort=80, managerPort=18080, ...) {
-    chmCreateServer (name,
-                     sprintf ("http://%s:%d/chm", intAddr, chmPort),
-                     list (serverProtocol="manager",
-                           deployServer=sprintf("http://%s:%d/chm/manager/rest", intAddr, managerPort),
-                           serviceName="default",
-                           viewServer=sprintf("http://%s:%d/chm", extAddr, chmPort),
-                           ...))
-}
